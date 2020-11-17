ebc1303df6024d626563587775b6e3b108283335
mezcel <mezcel@hotmail.com>
2020-11-17 08:56:49 -0500

    added a localhost alt url

--- index.go
+++ index.go
@@ -194,17 +194,22 @@ func WriteM3UPlaylist(m3uPlaylistPath string, Ip string, HostPort string) {
 // GetLocalIP returns the non loopback local IP of the host
 func GetLocalIP() string {
     addrs, err := net.InterfaceAddrs()
+
     if err != nil {
         return ""
     }
+
     for _, address := range addrs {
         // check the address type and if it is not a loopback the display it
         if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
             if ipnet.IP.To4() != nil {
                 return ipnet.IP.String()
             }
+        } else {
+            return "127.0.0.1"
         }
     }
+
     return ""
 }
 
